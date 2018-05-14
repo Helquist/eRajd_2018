3ae611a87fd31757e9d93c4c3721b1fcf5aceab9
Helquist <38138473+Helquist@users.noreply.github.com>
2018-05-14 22:59:57 +0200

    Wyświetlanie drabinki (bez dynamicznych teamów)

--- app/src/main/java/com/example/erajd_2018/VolleyballResultsFragment.java
+++ app/src/main/java/com/example/erajd_2018/VolleyballResultsFragment.java
@@ -20,7 +20,7 @@ public class VolleyballResultsFragment extends Fragment {
     public View text;
     public View currentGame;
     public View rootView;
-    public ViewGroup vg;
+    public ViewGroup tournamentBracketView;
 
 
     public VolleyballResultsFragment() {
@@ -41,8 +41,8 @@ public class VolleyballResultsFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_volleyball_results, container, false);
         currentGame = rootView.findViewById(R.id.currentGame);
-        vg = rootView.findViewById(R.id.tournament_bracket);
-        vg.addView(new DrawView(this.getActivity()));
+        tournamentBracketView = rootView.findViewById(R.id.tournament_bracket);
+        tournamentBracketView.addView(new DrawView(this.getActivity()));
 
         //container.addView(new DrawView(this.getActivity()));
 
@@ -74,11 +74,36 @@ public class VolleyballResultsFragment extends Fragment {
             paint.setStrokeWidth(8);
             super.onDraw(canvas);
 
-            underlineView(vg.findViewById(R.id.quarter_team1), canvas);
-            underlineView(rootView.findViewById(R.id.quarter_team2), canvas);
-            underlineView(rootView.findViewById(R.id.semi_team1), canvas);
-            connectView(rootView.findViewById(R.id.quarter_team1), rootView.findViewById(R.id.semi_team1), canvas);
-            connectView(rootView.findViewById(R.id.quarter_team2), rootView.findViewById(R.id.semi_team1), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team1), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team2), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team3), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team4), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team5), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team6), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team7), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.quarter_team8), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.semi_team1), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.semi_team2), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.semi_team3), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.semi_team4), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.final_team1), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.final_team2), canvas);
+            underlineView(tournamentBracketView.findViewById(R.id.winner_team), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team1), tournamentBracketView.findViewById(R.id.semi_team1), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team2), tournamentBracketView.findViewById(R.id.semi_team1), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team3), tournamentBracketView.findViewById(R.id.semi_team2), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team4), tournamentBracketView.findViewById(R.id.semi_team2), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team5), tournamentBracketView.findViewById(R.id.semi_team3), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team6), tournamentBracketView.findViewById(R.id.semi_team3), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team7), tournamentBracketView.findViewById(R.id.semi_team4), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.quarter_team8), tournamentBracketView.findViewById(R.id.semi_team4), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.semi_team1), tournamentBracketView.findViewById(R.id.final_team1), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.semi_team2), tournamentBracketView.findViewById(R.id.final_team1), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.semi_team3), tournamentBracketView.findViewById(R.id.final_team2), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.semi_team4), tournamentBracketView.findViewById(R.id.final_team2), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.final_team1), tournamentBracketView.findViewById(R.id.winner_team), canvas);
+            connectView(tournamentBracketView.findViewById(R.id.final_team2), tournamentBracketView.findViewById(R.id.winner_team), canvas);
+
 
 
         }
@@ -94,19 +119,26 @@ public class VolleyballResultsFragment extends Fragment {
 
         public void connectView(View view1, View view2, Canvas canvas){
 
+            //Pozioma linia
             int startX = view1.getRight();
             int startY = view1.getBottom();
-            int stopX = view2.getLeft();
+            int stopX = view2.getLeft()-10;
             int stopY = view1.getBottom();
             canvas.drawLine(startX, startY, stopX, stopY, paint);
 
-            startX = view2.getLeft();
+            //pionowa linia
+            startX = view2.getLeft()-10;
             startY = view1.getBottom();
-            stopX = view2.getLeft();
+            stopX = view2.getLeft()-10;
             stopY = view2.getBottom();
             canvas.drawLine(startX, startY, stopX, stopY, paint);
 
-
+            //Pozioma linia
+            startX = view2.getLeft()-10;
+            startY = view2.getBottom();
+            stopX = view2.getLeft();
+            stopY = view2.getBottom();
+            canvas.drawLine(startX, startY, stopX, stopY, paint);
 
 
         }
